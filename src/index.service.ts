65a0e54c62a1ae3e5be7bf030f33c53f256a7aad
James Pike <jpike@chilon.net>
2017-02-15 21:22:15 +0000

    simplify api with help of queueing-subject and share()

--- src/index.service.ts
+++ src/index.service.ts
@@ -1,24 +1,46 @@
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs/Observable'
+import { Subscription } from 'rxjs/Subscription'
+import { QueueingSubject } from 'queueing-subject'
+
+import 'rxjs/add/operator/share'
 
 @Injectable()
 export class WebSocketService {
-  connect(url: string, input: Observable<any>): Promise< Observable<any> > {
-    const socket = new WebSocket(url)
+  connect(url: string, input: Observable<any>): Observable<any> {
+    // wrap in a stream that will queue values until the socket is open
+    const inputQueue = new QueueingSubject<any>()
+    let inputSubscription = input.subscribe(data => { inputQueue.next(data) })
 
-    input.subscribe(data => { socket.send(JSON.stringify(data)) })
+    return new Observable<any>(observer => {
+      let inputQueueSubscription: Subscription
+      const socket = new WebSocket(url)
 
-    return new Promise((resolve, reject) => {
-      socket.onerror = reject
+      socket.onerror = error => {
+        inputSubscription.unsubscribe()
+        inputSubscription = null
+        observer.error(error)
+      }
+
+      socket.onmessage = message => {
+        observer.next(JSON.parse(message.data))
+      }
 
       socket.onopen = () => {
-        resolve(
-          Observable.create(observer => {
-            socket.onmessage = message => { observer.next(JSON.parse(message.data)) }
-            socket.onerror = observer.error.bind(observer)
-          })
-        )
+        inputQueueSubscription = inputQueue.subscribe(data => {
+          socket.send(JSON.stringify(data))
+        })
       }
-    })
+
+      socket.onclose = () => {
+        if (inputQueueSubscription) {
+          inputQueueSubscription.unsubscribe()
+          inputQueueSubscription = null
+        }
+        inputSubscription.unsubscribe()
+        inputSubscription = null
+        observer.complete()
+      }
+    }).share()
   }
 }
