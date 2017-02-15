e155296bff5787360744825888ad0ef7d95fab0b
James Pike <jpike@chilon.net>
2017-02-15 22:51:04 +0000

    do not enforce queueing-subject

--- src/index.service.ts
+++ src/index.service.ts
@@ -1,44 +1,40 @@
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs/Observable'
 import { Subscription } from 'rxjs/Subscription'
-import { QueueingSubject } from 'queueing-subject'
 
 import 'rxjs/add/operator/share'
 
 @Injectable()
 export class WebSocketService {
   connect(url: string, input: Observable<any>): Observable<any> {
-    // wrap in a stream that will queue values until the socket is open
-    const inputQueue = new QueueingSubject<any>()
-    let inputSubscription = input.subscribe(data => { inputQueue.next(data) })
-
     return new Observable<any>(observer => {
-      let inputQueueSubscription: Subscription
+      let inputSubscription: Subscription
       const socket = new WebSocket(url)
 
-      socket.onerror = error => {
-        inputSubscription.unsubscribe()
-        inputSubscription = null
-        observer.error(error)
+      const shutdown = () => {
+        if (inputSubscription) {
+          inputSubscription.unsubscribe()
+          inputSubscription = null
+        }
+      }
+
+      socket.onopen = () => {
+        inputSubscription = input.subscribe(data => {
+          socket.send(JSON.stringify(data))
+        })
       }
 
       socket.onmessage = message => {
         observer.next(JSON.parse(message.data))
       }
 
-      socket.onopen = () => {
-        inputQueueSubscription = inputQueue.subscribe(data => {
-          socket.send(JSON.stringify(data))
-        })
+      socket.onerror = error => {
+        shutdown()
+        observer.error(error)
       }
 
       socket.onclose = () => {
-        if (inputQueueSubscription) {
-          inputQueueSubscription.unsubscribe()
-          inputQueueSubscription = null
-        }
-        inputSubscription.unsubscribe()
-        inputSubscription = null
+        shutdown()
         observer.complete()
       }
     }).share()
