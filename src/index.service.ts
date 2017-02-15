91bf08ab821c93f8c4d84e97c9cb38fba83dd5af
James Pike <jpike@chilon.net>
2017-02-15 05:51:06 +0000

    return promise of observable that resolves post connection

--- src/index.service.ts
+++ src/index.service.ts
@@ -3,14 +3,22 @@ import { Observable } from 'rxjs/Observable'
 
 @Injectable()
 export class WebSocketService {
-  connect(url: string, input: Observable<any>): Observable<any> {
+  connect(url: string, input: Observable<any>): Promise< Observable<any> > {
     const socket = new WebSocket(url)
 
     input.subscribe(data => { socket.send(JSON.stringify(data)) })
 
-    return Observable.create(observer => {
-      socket.onmessage = message => { observer.next(JSON.parse(message.data)) }
-      socket.onerror = socket.onerror.bind(socket)
+    return new Promise((resolve, reject) => {
+      socket.onerror = reject
+
+      socket.onopen = () => {
+        resolve(
+          Observable.create(observer => {
+            socket.onmessage = message => { observer.next(JSON.parse(message.data)) }
+            socket.onerror = observer.error.bind(observer)
+          })
+        )
+      }
     })
   }
 }
